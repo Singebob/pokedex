6fce88307ea933a0124977b03b313e7556457c93
Jeremy Chauvin <jeremie.chauvin@epsi.fr>
2020-08-10 20:25:55 +0200

    start pokemon card

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+   <div class="q-pa-md row justify-center q-gutter-md">
+     <CardPokemon />
+     <CardPokemon />
+     <CardPokemon />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import CardPokemon from '@/components/cardPokemon.vue';
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'app',
+  components: {
+    CardPokemon,
+  },
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+};
+</script>
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 350px
 </style>
---
+++ src/components/cardPokemon.vue
@@ -0,0 +1,63 @@
+<template>
+   <div class="q-pa-md row items-start q-gutter-md">
+    <q-card class="my-card" flat bordered>
+      <q-img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+      />
+
+      <q-card-section>
+        <div class="text-h5 q-mt-sm q-mb-xs">bulbasaur</div>
+        <div class="row">
+          <div class="col">Poids: 320</div>
+          <div class="col">Taille: 11</div>
+        </div>
+      </q-card-section>
+
+      <q-card-actions>
+        <q-btn flat color="dark" label="Share" />
+        <q-btn flat color="primary" label="Book" />
+
+        <q-space />
+
+        <q-btn
+          color="grey"
+          round
+          flat
+          dense
+          :icon="expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
+          @click="expanded = !expanded"
+        />
+      </q-card-actions>
+
+      <q-slide-transition>
+        <div v-show="expanded">
+          <q-separator />
+          <q-card-section class="text-subitle2">
+            {{ lorem }}
+          </q-card-section>
+        </div>
+      </q-slide-transition>
+    </q-card>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'CardPokemon',
+
+  data() {
+    return {
+      expanded: false,
+      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    };
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.my-card
+  min-width: 250px
+  width: 100%
+  max-width: 350px
+</style>
